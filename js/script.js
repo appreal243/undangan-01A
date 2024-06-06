13f32a901eccc35095324c35f31f92906cc34478
appreal243 <apprgame243@gmail.com>
2024-06-07 01:32:11 +0700

    upload project undangan 01A

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const page1 = document.querySelector("section.page1");
+const btnOpen = document.querySelector(".btn-open");
+
+btnOpen.addEventListener("click", function () {});
+
+btnOpen.addEventListener("click", function () {
+  page1.style.top = "-100%";
+});
